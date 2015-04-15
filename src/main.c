2fbd90c811184ba4f4b690d0dd0969950aa9cfd8
ETumang <emily.tumang@gmail.com>
2015-04-14 21:29:00 -0400

    Port code back to PIC from arduino.

--- src/main.c
+++ src/main.c
@@ -12,22 +12,37 @@
 #define MOTOR_BASE_SPEED MAX_OUTPUT>>8
 
 #define SERVO 8
-#define KEYBOARD_CLOCK 7
-#define KEYBOARD_DATA 6
+#define KEYBOARD_CLOCK 12//green
+#define KEYBOARD_DATA 7//blue
 #define MOTOR 9
 
+#define FANS_1 10
+#define FANS_2 11
+#define FANS_3 13
+
+
+//for TS53 servo, change if we change servos
 #define INTERVAL .02
-#define MAXWIDTH .0024
+#define MAXWIDTH .003
 #define MINWIDTH .0006
 
+//control constants
 #define START 'b'
 #define END 'c'
 #define FLIP_SERVO 'd'
 #define SLOW_MOTOR 'e'
 #define ACCEL_MOTOR 'f'
 
+//motor speeds
 #define SLOWER 0
 #define FASTER 1
+
+//keyboard reading variables
+volatile uint8_t data;
+volatile uint8_t new_command = 0;
+volatile uint8_t started, bits;
+volatile int bit_count = 0;
+volatile int keyup = 0;//was the last code a keyup code?
 
 int flip(int flipped){
 	if(flipped){
@@ -52,6 +67,22 @@ uint16_t calc_speed(uint16_t speed, int m_dir){
 	}
 }
 
+int toggle_fans(int fanSet){
+	pin_clear(&D[FANS_1]);
+	pin_clear(&D[FANS_2]);
+	pin_clear(&D[FANS_3]);
+
+  	switch(fanSet){
+  		case FANS_1:
+  			pin_set(&D[FANS_1]);
+  		case FANS_2:
+  			pin_set(&D[FANS_2]);
+  		case FANS_3:
+  			pin_set(&D[FANS_3]);
+    }
+    return fanSet;
+}
+
 void main(void){
 	init_oc();
 	init_uart();
@@ -59,21 +90,18 @@ void main(void){
 	init_ui();
 	init_pin();
 
-	//keyboard values
-	volatile uint8_t data;
-	volatile uint8_t new_command = 0;
+	//switch controller, shouldn't do anything
 	char input = 'a';
-	uint8_t started, bits;
 
 	//keyboard pins
-	pin_digitalIn(&D[KEYBOARD_CLOCK]);//clock input
-	pin_digitalIn(&D[KEYBOARD_DATA]);//data input
+	pin_digitalIn(&D[KEYBOARD_CLOCK]);//clock input-blue
+	pin_digitalIn(&D[KEYBOARD_DATA]);//data input-green
 
 	//keyboard interrupt
 	INTCON2bits.INT4EP = 1;//interrupt on negative edge
 	INTCON1bits.NSTDIS = 1;//no nested interrupts
 	IEC3bits.INT4IE = 1;//enable interrupt
-	RPINR2bits.INT4R = 0xC;//set interrupt to RPin 7
+	RPINR2bits.INT4R = 0x17;//set interrupt to RPin 22
 
 	IFS3bits.INT4IF = 0;//clear flag
 
@@ -90,33 +118,43 @@ void main(void){
 	//motor constants
 	uint16_t speed = MOTOR_BASE_SPEED;
 
+	//fan outputs
+	pin_digitalOut(&D[FANS_1]);
+	pin_digitalOut(&D[FANS_2]);
+	pin_digitalOut(&D[FANS_3]);
+
+	pin_clear(&D[FANS_1]);
+	pin_set(&D[FANS_2]);
+	pin_clear(&D[FANS_3]);
+
 	while(1){
 		//read the keyboard
 		if(new_command){
-			input =  pgm_read_byte(&(keymap[data])); //grab character 				from array
+			input = keymap[data]; //grab character from array
+			uart_putc(&uart1, input);			
 			new_command = 0;
 		}
 		switch(input){
-			case START{
+			case START:
 				//start the timer
 				pin_set(&D[8]);
-			}
-			case END{
+				input = 'a';
+			case END:
 				//stop the timer
 				pin_clear(&D[8]);
 				pin_set(&D[8]);
-			}
-			case FLIP_SERVO{
+				input = 'a';
+			case FLIP_SERVO:
 				flipped = flip(flipped);
-			}
-			case ACCEL_MOTOR{
+				input = 'a';
+			case ACCEL_MOTOR:
 				speed = calc_speed(speed, FASTER);
-				pin_set(&D[MOTOR],speed);
-			}
-			case SLOW_MOTOR{
+				pin_write(&D[MOTOR],speed);
+				input = 'a';
+			case SLOW_MOTOR:
 				speed = calc_speed(speed, SLOWER);
-				pin_set(&D[MOTOR],speed);
-			}
+				pin_write(&D[MOTOR],speed);
+				input = 'a';
 		}
 	}
 }
@@ -126,21 +164,28 @@ void __attribute__((interrupt, no_auto_psv)) _INT4Interrupt(void){//keyboard int
 	if(!started){
 		if(!pin_read(&D[KEYBOARD_DATA])){
 			started = 1;
-			bit_count = 0;
 			data = 0;
+			new_command = 0;
 			return;
 		}
 	}
 	else if(bit_count<=8){
-		data |=(pin_read(&D[KEYBOARD_DATA])<<bit_count)
-	
+		data |=(pin_read(&D[KEYBOARD_DATA])<<bit_count);
 		bit_count++;
 		return;
 	}
 	else{
 		bit_count = 0;
 		started = 0;
-		new_command=1;
+		if(keyup){
+			keyup = 0;
+			return;
+		}
+		if(data == 240){
+			keyup = 1;
+			return;
+		}
+		new_command = 1;
 	}
 }
 
