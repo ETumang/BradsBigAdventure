8d2607c4ddf854a962155bfbb6de855d7e5deb2f
ETumang <emily.tumang@gmail.com>
2015-04-30 00:26:02 -0400

    Add code for all actuators

--- src/game_2.c
+++ src/game_2.c
@@ -7,58 +7,73 @@
 #include "pin.h"
 #include "keymap.h"
 
-//breakbeam interrupts
+//signal display to start
+#define DISPLAY 1
+
+//breakbeam interrupts- don't forget to attach the END interrupt to the arduino
 #define PENNY 2
 #define START 3
-#define END 4
 
 //actuator outputs
-#define SERVO_1 5
-#define SERVO_2 6
+#define SERVO_1 4
+#define SERVO_2 5
 
-#define BALL_SERVO 7
+#define BALL_SERVO 6
 
-#define MOTOR_1_R 8
-#define MOTOR_1_L 9
+#define MOTOR_1 7
 
-#define MOTOR_2_R 10
-#define MOTOR_2_L 11
+#define MOTOR_2 8
 
-#define MOTOR_3_R 12
-#define MOTOR_3_L 13
+#define MOTOR_3 9
 
+#define MOTOR_4 0
+
+//Inputs
+#define SERVO_1_IN 10
+#define SERVO_2_IN 11
+
+#define MOTOR_1_FAST 12
+#define MOTOR_1_SLOW 13
+//repurposed analog pins
+#define MOTOR_2_FAST 0
+#define MOTOR_2_SLOW 1
+#define MOTOR_3_FAST 2
+#define MOTOR_3_SLOW 3
+#define MOTOR_4_FAST 4
+#define MOTOR_4_SLOW 5
 
 //for TS53 servo, change if we change servos
 #define INTERVAL .02
 #define MAXWIDTH .003
 #define MINWIDTH .0006
 
-//motor speeds
-#define SLOWER 0
-#define FASTER 1
+#define NUM_SERVOS 2
+#define NUM_MOTORS_ANALOG 3
 
-int flip(int flipped){
+int flip(int flipped, int pin){
 	if(flipped){
-		pin_write(&D[SERVO],65534);
+		pin_write(&D[pin],65534);
 		led_on(&led2);
 		return 0;
 	}
 	else{		
 		led_off(&led2);
-		pin_write(&D[SERVO],0);
+		pin_write(&D[pin],0);
 		return 1;
 	}
 }
 
-int slow(int speed){
+
+//Edit this
+int slow(int speed, int pin){
 	if(speed){
-		pin_write(&D[MOTOR],65534);
+		pin_write(&D[pin],65534);
 		led_on(&led2);
 		return 0;
 	}
 	else{		
 		led_off(&led2);
-		pin_write(&D[MOTOR],0);
+		pin_write(&D[pin],0);
 		return 1;
 	}
 }
@@ -72,21 +87,43 @@ void main(void){
 	init_ui();
 	init_pin();
 
+	//interrupt triggered
 	pin_digitalOut(&D[DISPLAY]);
-	pin_digitalOut(&D[SERVO]);
-	pin_digitalOut(&D[MOTOR]);
 	pin_digitalOut(&D[BALL_SERVO]);
-	//pin_clear(&D[MOTOR]);
 
+	//servos
+	pin_digitalOut(&D[SERVO_1]);
+	pin_digitalOut(&D[SERVO_2]);
+
+	//motors
+	pin_digitalOut(&D[MOTOR_1]);
+	pin_digitalOut(&D[MOTOR_2]);
+	pin_digitalOut(&D[MOTOR_3]);
+	pin_digitalOut(&D[MOTOR_4]);
+
+	//interrupts
 	pin_digitalIn(&D[PENNY]);
 	pin_digitalIn(&D[START]);
-	pin_digitalIn(&D[END]);
 
-	pin_digitalIn(&D[SERVO_IN]);
+	pin_digitalIn(&D[SERVO_1_IN]);
+	pin_digitalIn(&D[SERVO_2_IN]);
 
-	//pin_analogIn(&A[MOTOR_IN]);
+	pin_digitalIn(&D[MOTOR_1_SLOW]);
+	pin_digitalIn(&D[MOTOR_1_FAST]);
 
-	int done = 0;
+	pin_digitalIn(&A[MOTOR_2_SLOW]);
+	pin_digitalIn(&A[MOTOR_2_FAST]);
+	pin_digitalIn(&A[MOTOR_3_SLOW]);
+	pin_digitalIn(&A[MOTOR_3_FAST]);
+	pin_digitalIn(&A[MOTOR_4_SLOW]);
+	pin_digitalIn(&A[MOTOR_4_FAST]);
+
+	int servo_inputs[2] = {SERVO_1_IN, SERVO_2_IN};
+	int servo_outputs[2] = {SERVO_1, SERVO_2};
+
+	int motor_inputs[6] = {MOTOR_2_FAST,MOTOR_2_SLOW, MOTOR_3_FAST,MOTOR_3_SLOW,MOTOR_4_FAST,MOTOR_4_SLOW};
+	//does not include motor 1 because motor 1 reads from a digital pin
+	int motor_outputs[3] = {MOTOR_2, MOTOR_3, MOTOR_4};
 
 	//game start interrupt
 	INTCON2bits.INT2EP = 1;//interrupt on negative edge
@@ -95,14 +132,6 @@ void main(void){
 	RPINR1bits.INT2R = 0xA;//set interrupt to RPin 10 (digital pin 2)
 
 	IFS1bits.INT2IF = 0;//clear flag
-
-	//game end interrupt
-	INTCON2bits.INT3EP = 1;//interrupt on negative edge
-	//INTCON1bits.NSTDIS = 1;//no nested interrupts
-	IEC3bits.INT3IE = 1;//enable interrupt
-	RPINR1bits.INT3R = 0x11;//set interrupt to RPin17 (digital pin 3)
-
-	IFS3bits.INT3IF = 0;//clear flag
 
 	//penny insertion interrupt
 	INTCON2bits.INT4EP = 1;//interrupt on negative edge
@@ -113,34 +142,58 @@ void main(void){
 	IFS3bits.INT4IF = 0;//clear flag
 
 	//servo values
-	int flipped = 0;
+	int flipped[2] = {0,0};//neither servo is flipped
 
 	//servo pins
-	oc_servo(&oc2,&D[SERVO], &timer1,INTERVAL,MINWIDTH,MAXWIDTH,0);//gameplay servo
+	oc_servo(&oc1,&D[SERVO_1], NULL,INTERVAL,MINWIDTH,MAXWIDTH,0);//gameplay servo
+	oc_servo(&oc2,&D[SERVO_1], NULL,INTERVAL,MINWIDTH,MAXWIDTH,0);
 	oc_servo(&oc3,&D[BALL_SERVO],&timer2,INTERVAL,MINWIDTH,MAXWIDTH,0);//ball dispensing servo
 	// oc_servo(&oc4,&D[SERVO],&timer3,INTERVAL,MINWIDTH,MAXWIDTH,0);
 
 	//motor driving OC
-	oc_pwm(&oc1, &D[MOTOR], NULL, 1000, 0);
-	int speed = 1;
-
-	pin_digitalOut(&D[11]);
-	pin_write(&D[11],0);
+	oc_pwm(&oc4, &D[MOTOR_1], NULL, 1000, 0);
+	oc_pwm(&oc5, &D[MOTOR_2], NULL, 1000, 0);
+	oc_pwm(&oc6, &D[MOTOR_3], NULL, 1000, 0);
+	oc_pwm(&oc7, &D[MOTOR_4], NULL, 1000, 0);
 
 	//motor variables
-
+	int motor_speeds[4] = {0,0,0,0};
 	led_on(&led1);
+
+
+	int i = 0;
 
 	while(1){
 		pin_set(&D[DISPLAY]);
 		if(active){
+			//read servo inputs and react
+			for(i = 0; i < NUM_SERVOS; i++){
+				if(!pin_read(&D[(servo_inputs[i])])){
+					flipped[i] = flip(flipped[i],servo_inputs[i]);
+				}
+			}
 
-			if(!pin_read(&D[MOTOR_IN])){
-				speed = slow(speed);
+			//read motor 2-4 inputs and react
+			for(i = 0; i < NUM_MOTORS_ANALOG; i++){
+				if(!pin_read(&A[motor_inputs[i]])){
+					if(i%2 == 0){
+						//division maps each input pin to correct output, since there are 2 inputs/motor
+						motor_speeds[i] = slow(1,motor_inputs[((int)(i/2))]);
+					}
+					else{
+						motor_speeds[i] = slow(0, motor_inputs[((int)(i/2))]);
+					}
+				}
 			}
-			if(!pin_read(&D[SERVO_IN])){
-				flipped = flip(flipped);
+
+			//read motor 1 and react
+			if(!pin_read(&D[MOTOR_1_FAST])){
+				motor_speeds[3] = slow(1,MOTOR_1);
 			}
+			if(!pin_read(&D[MOTOR_1_SLOW])){
+				motor_speeds[3] = slow(0, MOTOR_1);
+			}
+
 		}
 	}
 }
@@ -159,15 +212,9 @@ void __attribute__((interrupt, no_auto_psv)) _INT3Interrupt(void){//game start i
 	pin_clear(&D[DISPLAY]);
 	led_toggle(&led2);
 	pin_write(&D[BALL_SERVO],0);
+	active = 1;
 }
 
 
-void __attribute__((interrupt, no_auto_psv)) _INT4Interrupt(void){//game loss interrupt
-
-	IFS3bits.INT4IF = 0;//clear flag
-	pin_clear(&D[DISPLAY]);
-	//active = 0;
-	led_toggle(&led3);
-}
 
 	
